61a7342696c82d94284ec1342015d1521f00948c
Ronald Volgers <rvolgers@users.noreply.github.com>
2019-04-26 09:44:46 +0200

    Add a test bindfstab and a basic seccomp profile
    These are used in the dev environment, so you can test again just by doing "cargo run" inside the dev container.

--- dev.sh
+++ dev.sh
@@ -2,6 +2,6 @@
 
 docker build --iidfile ./iidfile .
 
-docker run --rm -it -v "$(pwd):/home/user/docker-localbind" --security-opt seccomp=unconfined -w /home/user/docker-localbind $(cat ./iidfile) /bin/bash
+docker run --rm -it -v "$(pwd)/test-bindfstab:/etc/bindfstab" -v "$(pwd):/home/user/docker-localbind" --security-opt seccomp="$(pwd)/docker-localbind-seccomp-profile.json" -w /home/user/docker-localbind $(cat ./iidfile) /bin/bash
 
 
